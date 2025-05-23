771f5c56993fc679bd8be508bf44226598104eaa
Abdullah <abdullahmakhous@gmail.com>
2025-05-24 00:42:51 +0300

    basic game structure with only screen setting and an empty game loop

---
+++ src/main/Game.java
@@ -0,0 +1,28 @@
+package main;
+
+import javax.swing.JFrame;
+
+class Game {
+
+	public static void main(String[] args) {
+		
+		//this is going to be the main window
+		JFrame window = new JFrame();
+		 
+		//some basic setting 
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //setting exit button
+		window.setResizable(false); //we can't resize the window
+		window.setTitle("GAME"); //naming the window
+		window.setVisible(true); //so we can see the window
+		//we don't need this because we will depend on the panel screen setting
+		//window.setSize(new Dimension(500,500)); //setting window's dimensions
+		 
+		GamePanel gamePanel = new GamePanel(); //creating game panel object
+		window.add(gamePanel); //adding the panel so its displayed in the window 
+		window.pack();//to apply size setting
+		
+		window.setLocationRelativeTo(null); //to center the window + this needs to be below
+
+	}
+
+}
---
+++ src/main/GamePanel.java
@@ -0,0 +1,76 @@
+package main;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import javax.swing.JPanel;
+
+//this class represent the game panel so actions will be done here
+//panel is added to the jFrame 
+//its a subclass of jPanel so we can override its methods
+//it implements runnable because it contains game loop so we need to create a game thread
+
+class GamePanel extends JPanel implements Runnable{
+ 
+	//size configurations
+	final int originalTileSize = 16; //16x16 pixels
+	final int scale = 3; //we will need this to adjust size (16 x 3 = 48)
+    
+	final int tileSize = scale * originalTileSize;//the actual size 48x48
+	
+	final int maxScreenCol = 16; //16 pixels vertically 
+	final int maxScreenRow = 12; //16 pixels horizontally 
+	
+	final int screenWidth = tileSize * maxScreenCol; //total width 768 pixels
+	final int screenHeigth = tileSize * maxScreenRow; //total Height 576 pixels
+	
+	public GamePanel() {
+		this.setPreferredSize(new Dimension(screenWidth,screenHeigth));//size setting
+		this.setBackground(Color.black);//setting background color
+		this.setDoubleBuffered(true);//advanced
+	} 
+	
+	/*to set the game cycle we need to work with thread
+	to get in touch with the fourth dimension(time)*/
+	
+	Thread gameThread;//to create a thread object 
+	
+	public void startGameThread() {
+		
+		gameThread = new Thread(this); //we pass the panel because run method is now one of its sub methods
+		gameThread.start(); //بسم الله الرحمن الرحيم
+		
+	}
+	//game loop is inside the run method
+	@Override
+	public void run() {	
+		// game loop will keep repeating this as long as the game thread is alive
+		while(gameThread != null) {
+		//game loop consists of three stages
+		
+			//1.Update : update the game status such as players attributes (health, position...etc)
+			update();
+			//2.Draw : draw the screen with the updated frame information
+			repaint();
+			//
+		}
+	}
+	public void update() {
+		// TODO Auto-generated method stub
+		 
+	}
+	
+	//this is the draw method but we can't rename it because this is a java method
+	public void paintComponent(Graphics g) {
+		// call the java method 
+		super.paintComponent(g);
+
+		Graphics2D g2 = (Graphics2D) g; //convert the received graphics to 2d (usual procedure) because Graphics2d has some good functions  
+		g2.setColor(Color.white); // set the color of the component
+		g2.fillRect(100, 100, tileSize, tileSize);//draw a rectangle just for testing purposes 
+		
+		g2.dispose(); //cleaning component to stay memory efficient 
+	}
+	 
+}
